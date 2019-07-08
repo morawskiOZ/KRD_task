323231e6c2fdc9eff8ff2cc42919d059a24b7cc4
Piotr Morawski <piotrmorawski90@gmail.com>
2019-07-08 23:20:23 +0200

    add build to deploy

---
+++ build/precache-manifest.443aad1bb1229043153d0783d19b36a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62382c396d15e778917fa33db7c8422a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79aa6ee0d0c594d14d3",
+    "url": "/static/css/main.57ecaf85.chunk.css"
+  },
+  {
+    "revision": "fd19a306af583f917409",
+    "url": "/static/js/2.eafe2cc4.chunk.js"
+  },
+  {
+    "revision": "d79aa6ee0d0c594d14d3",
+    "url": "/static/js/main.c6507131.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
